46e18575968b1ad108bc358d51d28b1ab32c05e4
Jordan Wallace-Addai <you@example.com>
2019-10-24 19:16:42 -0400

    database config update

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
 import * as firebase from 'firebase';
+import '@firebase/firestore';
 
-// Initialize Firebaseconst
-const firebaseConfig = {
-  apiKey: 'AIzaSyDFVjaGy--PoJVT4tO5UpLEvpTJ1U8BZ8g',
-  authDomain: 'reactfirebaseassignment.firebaseapp.com',
-  databaseURL: 'https://reactfirebaseassignment.firebaseio.com',
-  projectId: 'reactfirebaseassignment',
-  storageBucket: 'reactfirebaseassignment.appspot.com',
-  messagingSenderId: '99206323679',
-  appId: '1:99206323679:web:559111c98d67cddd45b86a'
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: 'AIzaSyAdg845RaKUaElk8paiPVF5k6K5lwQ8Fxk',
+  authDomain: 'reactfirebaseassignment-3af19.firebaseapp.com',
+  databaseURL: 'https://reactfirebaseassignment-3af19.firebaseio.com',
+  projectId: 'reactfirebaseassignment-3af19',
+  storageBucket: 'reactfirebaseassignment-3af19.appspot.com',
+  messagingSenderId: '652934914254',
+  appId: '1:652934914254:web:c0b2d898fa4f0e7c51309f'
 };
 
 firebase.initializeApp(firebaseConfig);
